6ece07585168b37e03327d97c1826b141c156091
Leonardo Anjos <leodrums11@gmail.com>
2019-01-20 01:26:11 -0300

    simple very mush interactive script

---
+++ getting-started/interactive.sh
@@ -0,0 +1,13 @@
+#! /bin/bash 
+
+echo "hey! what is you first name?";
+read first_name;
+
+echo "welcome mr./mrs. $first_name , would you like to tell us, your last name";
+read last_name;
+
+echo thanks mr./mrs $first_name $last_name for telling us your name;
+echo "**************************************************************";
+echo "mr./mrs $last_name, it is time to say youn good bye";
+
+
